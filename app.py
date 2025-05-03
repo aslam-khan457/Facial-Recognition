297aa78deed5cd2a96099d051e89ad8f3eca9c26
aslam-khan457 <aslamkhan901215@gmail.com>
2025-05-03 09:25:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def process_frame():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
+port = int(os.environ.get("PORT", 8080))
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host="0.0.0.0", port=port)
